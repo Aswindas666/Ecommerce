01e9e20a6b72a11cbdca7ff25ee606fae6ac4e9b
ASWIN DAS PM <aswindaspm2@gmail.com>
2023-04-11 11:20:27 +0530

    changes 2

---
+++ static/app/js/myscripts.js
@@ -0,0 +1,95 @@
+
+
+
+$('.plus-cart').click(function(){
+    var id=$(this).attr("pid").toString();
+    var eml = (this.parentNode.children[2])
+    console.log("pid =",id)
+    $.ajax({
+        type:"GET",
+        url:"/pluscart",
+        data:{
+            prod_id:id
+        },
+        success:function(data){
+            console.log("data = ",data);
+            eml.innerText=data.quantity
+            document.getElementById("amount").innerText=data.amount
+            document.getElementById("totalamount").innerText=data.totalamount
+        }
+    
+    })
+})
+
+$('.minus-cart').click(function(){
+    var id=$(this).attr("pid").toString();
+    var eml = (this.parentNode.children[2])
+    // console.log("pid =",id)
+    $.ajax({
+        type:"GET",
+        url:"/minuscart",
+        data:{
+            prod_id:id
+        },
+        success:function(data){
+            // console.log("data = ",data);
+            eml.innerText=data.quantity
+            document.getElementById("amount").innerText=data.amount
+            document.getElementById("totalamount").innerText=data.totalamount
+        }
+    
+    })
+})
+
+$('.remove-cart').click(function(){
+    var id=$(this).attr("pid").toString();
+    var eml = this    // console.log("pid =",id)
+    $.ajax({
+        type:"GET",
+        url:"/removecart",
+        data:{
+            prod_id:id
+        },
+        success:function(data){
+            document.getElementById("amount").innerText=data.amount
+            document.getElementById("totalamount").innerText=data.totalamount
+            eml.parentNode.parentNode.parentNode.parentNode.romove()
+            // location.reload();
+            window.location.href='http://k2dietkit.com/cart/'
+        }
+    
+    })
+})
+
+
+$('.plus-wishlist').click(function(){
+    var id=$(this).attr("pid").toString();
+    $.ajax({
+        type:"GET",
+        url:"/pluswishlist",
+        data:{
+            prod_id:id
+        },
+        success:function(data){
+            //alert(data.message)
+            window.location.href='http://k2dietkit.com/product_detail/'+id+'/';
+        }
+    
+    })
+})
+
+$('.minus-wishlist').click(function(){
+    var id=$(this).attr("pid").toString();
+    $.ajax({
+        type:"GET",
+        url:"/minuswishlist",
+        data:{
+            prod_id:id
+        },
+        success:function(data){
+            //alert(data.message)
+            window.location.href='http://k2dietkit.com/product_detail/'+id+'/';
+        }
+        // http://127.0.0.1:8000
+    })
+})
